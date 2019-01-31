681e4b27e15ddc63b9ebbd2e09a8b4a6a74f8e44
cperry92 <42746925+cperry92@users.noreply.github.com>
2019-01-31 18:34:06 -0500

    add likemanagement test to main branch

---
+++ chirpbook-api/test/database/test-likemanagement.js
@@ -0,0 +1,21 @@
+const {LikeManagement, assert} = require("../common");
+
+it('test add like', function(done)
+{
+    LikeManagement.addLike(0,0,0,function(result)
+    {
+        console.log(result)
+        assert.strictEqual(result.rowCount, 1)
+        done();
+    });
+});
+
+it('test remove like', function(done)
+{
+    LikeManagement.removeLike(0,0, function(result)
+    {
+        console.log(result)
+        assert.strictEqual(result.rowCount, 1)
+        done();
+    });
+});
