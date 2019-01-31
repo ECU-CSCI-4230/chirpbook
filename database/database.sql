ea38fca2122c32b12ebfb65669798c9d14987443
Zachary Catalano <catalanoz15@students.ecu.edu>
2019-01-31 16:46:24 -0500

    Fix database not updating when SQL is changed

--- database/database.sql
+++ database/database.sql
@@ -1,8 +1,8 @@
-CREATE DATABASE cbook
-    WITH 
-    OWNER = postgres
-    ENCODING = 'UTF8'
-    CONNECTION LIMIT = -1;
+-- CREATE DATABASE cbook
+--     WITH
+--     OWNER = postgres
+--     ENCODING = 'UTF8'
+--     CONNECTION LIMIT = -1;
 
 CREATE TABLE public."User"
 (
@@ -26,128 +26,185 @@ CREATE TABLE public."Post"
     postid bigserial NOT NULL,
     userid bigint NOT NULL,
     post_text character varying(512) NOT NULL,
-    time_posted timestamp with time zone NOT NULL DEFAULT now(),
-    PRIMARY KEY (postid),
-    CONSTRAINT userid FOREIGN KEY (userid)
-        REFERENCES public."User" (userid) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
+    time_posted timestamp
+    with time zone NOT NULL DEFAULT now
+    (),
+    PRIMARY KEY
+    (postid),
+    CONSTRAINT userid FOREIGN KEY
+    (userid)
+        REFERENCES public."User"
+    (userid) MATCH SIMPLE
+        ON
+    UPDATE NO ACTION
+        ON
+    DELETE NO ACTION
 )
-WITH (
+    WITH
+    (
     OIDS = FALSE
 );
 
-ALTER TABLE public."Post"
+    ALTER TABLE public."Post"
     OWNER to postgres;
 
-CREATE TABLE public."Comment"
-(
-    commentid bigserial NOT NULL,
-    postid bigint NOT NULL,
-    parent_comment bigint,
-    userid bigint NOT NULL,
-    comment_text character varying(512) COLLATE pg_catalog."default" NOT NULL,
-    time_posted timestamp with time zone NOT NULL DEFAULT now(),
+    CREATE TABLE public."Comment"
+    (
+        commentid bigserial NOT NULL,
+        postid bigint NOT NULL,
+        parent_comment bigint,
+        userid bigint NOT NULL,
+        comment_text character varying(512) COLLATE pg_catalog
+        ."default" NOT NULL,
+    time_posted timestamp
+        with time zone NOT NULL DEFAULT now
+        (),
     deleted boolean NOT NULL,
-    CONSTRAINT "Comment_pkey" PRIMARY KEY (commentid),
-    CONSTRAINT postid FOREIGN KEY (postid)
-        REFERENCES public."Post" (postid) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION,
-    CONSTRAINT userid FOREIGN KEY (userid)
-        REFERENCES public."User" (userid) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
+    CONSTRAINT "Comment_pkey" PRIMARY KEY
+        (commentid),
+    CONSTRAINT postid FOREIGN KEY
+        (postid)
+        REFERENCES public."Post"
+        (postid) MATCH SIMPLE
+        ON
+        UPDATE NO ACTION
+        ON
+        DELETE NO ACTION,
+    CONSTRAINT userid
+        FOREIGN KEY
+        (userid)
+        REFERENCES public."User"
+        (userid) MATCH SIMPLE
+        ON
+        UPDATE NO ACTION
+        ON
+        DELETE NO ACTION
 )
-WITH (
+        WITH
+        (
     OIDS = FALSE
 )
 TABLESPACE pg_default;
 
-ALTER TABLE public."Comment"
+        ALTER TABLE public."Comment"
     OWNER to postgres;
 
-CREATE TABLE public."Like"
-(
-    postid bigint NOT NULL,
-    userid bigint NOT NULL,
-    PRIMARY KEY (postid, userid),
-    CONSTRAINT postid FOREIGN KEY (postid)
-        REFERENCES public."Post" (postid) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION,
-    CONSTRAINT userid FOREIGN KEY (userid)
-        REFERENCES public."User" (userid) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
+        CREATE TABLE public."Like"
+        (
+            postid bigint NOT NULL,
+            userid bigint NOT NULL,
+            PRIMARY KEY (postid, userid),
+            CONSTRAINT postid FOREIGN KEY (postid)
+        REFERENCES public."Post" (postid)
+            MATCH SIMPLE
+        ON
+            UPDATE NO ACTION
+        ON
+            DELETE NO ACTION,
+    CONSTRAINT userid
+            FOREIGN KEY
+            (userid)
+        REFERENCES public."User"
+            (userid) MATCH SIMPLE
+        ON
+            UPDATE NO ACTION
+        ON
+            DELETE NO ACTION
 )
-WITH (
+            WITH
+            (
     OIDS = FALSE
 );
 
-ALTER TABLE public."Like"
+            ALTER TABLE public."Like"
     OWNER to postgres;
 
-CREATE TABLE public."Dislike"
-(
-    postid bigint NOT NULL,
-    userid bigint NOT NULL,
-    PRIMARY KEY (postid, userid),
-    CONSTRAINT postid FOREIGN KEY (postid)
-        REFERENCES public."Post" (postid) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION,
-    CONSTRAINT userid FOREIGN KEY (userid)
-        REFERENCES public."User" (userid) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
+            CREATE TABLE public."Dislike"
+            (
+                postid bigint NOT NULL,
+                userid bigint NOT NULL,
+                PRIMARY KEY (postid, userid),
+                CONSTRAINT postid FOREIGN KEY (postid)
+        REFERENCES public."Post" (postid)
+                MATCH SIMPLE
+        ON
+                UPDATE NO ACTION
+        ON
+                DELETE NO ACTION,
+    CONSTRAINT userid
+                FOREIGN KEY
+                (userid)
+        REFERENCES public."User"
+                (userid) MATCH SIMPLE
+        ON
+                UPDATE NO ACTION
+        ON
+                DELETE NO ACTION
 )
-WITH (
+                WITH
+                (
     OIDS = FALSE
 );
 
-ALTER TABLE public."Dislike"
+                ALTER TABLE public."Dislike"
     OWNER to postgres;
 
-CREATE TABLE public."Friend"
-(
-    user1 bigint NOT NULL,
-    user2 bigint NOT NULL,
-    PRIMARY KEY (user1, user2),
-    CONSTRAINT user1 FOREIGN KEY (user1)
-        REFERENCES public."User" (userid) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION,
-    CONSTRAINT user2 FOREIGN KEY (user2)
-        REFERENCES public."User" (userid) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
+                CREATE TABLE public."Friend"
+                (
+                    user1 bigint NOT NULL,
+                    user2 bigint NOT NULL,
+                    PRIMARY KEY (user1, user2),
+                    CONSTRAINT user1 FOREIGN KEY (user1)
+        REFERENCES public."User" (userid)
+                    MATCH SIMPLE
+        ON
+                    UPDATE NO ACTION
+        ON
+                    DELETE NO ACTION,
+    CONSTRAINT user2
+                    FOREIGN KEY
+                    (user2)
+        REFERENCES public."User"
+                    (userid) MATCH SIMPLE
+        ON
+                    UPDATE NO ACTION
+        ON
+                    DELETE NO ACTION
 )
-WITH (
+                    WITH
+                    (
     OIDS = FALSE
 );
 
-ALTER TABLE public."Friend"
+                    ALTER TABLE public."Friend"
     OWNER to postgres;
 
-CREATE TABLE public."Friend_Request"
-(
-    sender bigint NOT NULL,
-    receiver bigint NOT NULL,
-    PRIMARY KEY (sender, receiver),
-    CONSTRAINT sender FOREIGN KEY (sender)
-        REFERENCES public."User" (userid) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION,
-    CONSTRAINT receiver FOREIGN KEY (receiver)
-        REFERENCES public."User" (userid) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
+                    CREATE TABLE public."Friend_Request"
+                    (
+                        sender bigint NOT NULL,
+                        receiver bigint NOT NULL,
+                        PRIMARY KEY (sender, receiver),
+                        CONSTRAINT sender FOREIGN KEY (sender)
+        REFERENCES public."User" (userid)
+                        MATCH SIMPLE
+        ON
+                        UPDATE NO ACTION
+        ON
+                        DELETE NO ACTION,
+    CONSTRAINT receiver
+                        FOREIGN KEY
+                        (receiver)
+        REFERENCES public."User"
+                        (userid) MATCH SIMPLE
+        ON
+                        UPDATE NO ACTION
+        ON
+                        DELETE NO ACTION
 )
-WITH (
+                        WITH
+                        (
     OIDS = FALSE
 );
 
-ALTER TABLE public."Friend_Request"
+                        ALTER TABLE public."Friend_Request"
     OWNER to postgres;
-
