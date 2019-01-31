c9ed404d516aa21a68bdb38e608fc6e298100f0b
cperry92 <42746925+cperry92@users.noreply.github.com>
2019-01-31 18:32:14 -0500

    add likemanagement to main branch

---
+++ chirpbook-api/src/likemanagement.js
@@ -0,0 +1,53 @@
+const db = require('./config/database')
+
+var log = require('console-log-level')({level: 'warn'})
+
+class LikeManagement
+{
+    static addLike(postid, userid, liketype, callback)
+    {
+	db.connect(function(client)
+        {
+            client.query(`INSERT INTO public."Like_Dislike" (postid, userid, liketype)
+								VALUES ($1, $2, $3)`, [postid, userid, liketype],
+                function(err, result)
+                {
+                    client.release()
+                    if(err)
+                    {
+                        log.error(err)
+                    }
+                    if(result)
+                    {
+                        log.info(result)
+                        callback(result)
+                    }
+                });
+        });
+    }
+
+    static removeLike(postid, userid, callback)
+    {
+        db.connect(function(client)
+        {
+            client.query(`DELETE FROM public."Like_Dislike" WHERE postid == $1 AND userid == $2)`,
+                         [postid, userid],
+                function(err, result)
+                {
+                    client.release()
+                    if(err)
+                    {
+                        log.error(err)
+                    }
+                    if(result)
+                    {
+                        log.info(result)
+                        callback(result)
+                    }
+                });
+        });
+    }
+
+}
+
+module.exports = LikeManagement
