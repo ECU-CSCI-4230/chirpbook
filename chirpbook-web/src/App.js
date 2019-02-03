2c2f0535861ceafd6953fc0e8553ecac1ed5f1f3
Zachary Catalano <catalanoz15@students.ecu.edu>
2019-02-03 14:46:21 -0500

    Implement router and add basic homepage

--- chirpbook-web/src/App.js
+++ chirpbook-web/src/App.js
@@ -1,77 +1,30 @@
-import React, { Component } from 'react';
-import { GoogleLogin, GoogleLogout } from 'react-google-login';
-import config from './config.json';
+import React from "react";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
-class App extends Component {
+import NavBar from './Components/Navbar';
+import Homepage from './Components/Homepage';
 
-    constructor() {
-        super();
-        this.state = { isAuthenticated: false, user: null, token: ''};
-        //this.fetch = this.fetch.bind(this)
-    }
+// import Loadable from "react-loadable";
 
-    logout = () => {
-        this.setState({isAuthenticated: false, token: '', user: null})
-    };
+// function Loading()
+// {
+//     return <h3>Loading...</h3>;
+// }
 
-    onFailure = (error) => {
-        alert('error');
-        console.log(this.state)
-        console.log(error)
-        console.log(config)
-    };
-
-
-    googleResponse = (response) => {
-        const token = JSON.stringify({'idToken': response.tokenId})
-        //{type : 'application/json'});
-        console.log('sucess')
-        console.log(response)
-        const options = {
-            headers: {'Content-Type': 'application/json'},
-            method: 'POST',
-            body: token,
-        };
-        //const tokenH = r.headers.get('x-auth-token');
-        fetch('http://localhost/api/v1/auth/google', options).then(r => r.json())
-        .then(data =>{
-            console.log(data.email)
-            this.setState({isAuthenticated: true, user: data.email})
-        });
-
-    };
-
-    render() {
-    let content = !!this.state.isAuthenticated ?
-            (
-                <div>
-                    <p>Authenticated {this.state.user.email}</p>
-                    <div>
-                        <GoogleLogout
-                            buttonText="Logout"
-                            onLogoutSuccess={this.logout}
-                        >
-                        </GoogleLogout>
-                    </div>
-                </div>
-            ) :
-            (
-                <div>
-                    <p>HI</p>
-                    <GoogleLogin
-                        clientId={config.GOOGLE_CLIENT_ID}
-                        buttonText="Login"
-                        
-                        onSuccess={this.googleResponse}
-                        onFailure={this.onFailure}
-                    />
-                </div>
-            );
-
+class App extends React.Component
+{
+    render()
+    {
         return (
-            <div className="App">
-                {content}
-            </div>
+            <Router>
+                <React.Fragment>
+
+                    <Route component={NavBar} />
+                    <Switch >
+                        <Route exact path="/" component={Homepage} />
+                    </Switch>
+                </React.Fragment >
+            </Router>
         );
     }
 }
