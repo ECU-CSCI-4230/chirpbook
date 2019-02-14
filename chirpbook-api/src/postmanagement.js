5b8184d1293806af2d4c648c747f5ba72b4033cf
Zachary Catalano <catalanoz15@students.ecu.edu>
2019-02-14 17:23:16 -0500

    Make getFriendPosts use email instead of userid

--- chirpbook-api/src/postmanagement.js
+++ chirpbook-api/src/postmanagement.js
@@ -30,31 +30,6 @@ class PostManagement
         });
     }
 
-    static getUserPosts(userid, cb)
-    {
-        db.connect(function(client)
-        {
-            client.query(`SELECT postid, post_text, time_posted FROM public."Post"
-                            WHERE userid = $1`, [userid],
-                function(err, result)
-                {
-                    client.release();
-                    if(err)
-                    {
-                        log.error(err);
-                    }
-                    if(result)
-                    {
-                        log.info(result);
-                        cb(result);
-                    } else
-                    {
-                        cb({rows: [], rowCount: 0});
-                    }
-                });
-        });
-    }
-
     static getPost(postid, cb)
     {
         db.connect(function(client)
@@ -211,24 +186,28 @@ class PostManagement
         });
     }
 
-    static getFriendPosts(userid, cb)
+    static getFriendPosts(email, cb)
     {
         db.connect(function(client)
         {
-            client.query(`SELECT public."Post".userid, public."Post".postid, post_text, time_posted, COALESCE(likes, 0) as likes, COALESCE(dislikes,0) as dislikes FROM public."Post"
-            NATURAL LEFT JOIN
-            (SELECT count(liketype) as likes, postid FROM public."Like_Dislike"
-              WHERE postid in (SELECT postid FROM public."Post") and liketype = 1
-              GROUP BY postid
-            ) as likeQuery NATURAL LEFT JOIN
-            (SELECT count(liketype) as dislikes, postid FROM public."Like_Dislike"
-              WHERE postid in (SELECT postid FROM public."Post") and liketype = 0
-              GROUP BY postid
-            ) as dislikeQuery
-              WHERE userid in ((SELECT user2 as user FROM public."Friend" WHERE user1 = $1)
-                      UNION (SELECT user1 as user FROM public."Friend" WHERE user2 = $1)) or userid = $1
-
-             `, [userid],
+            client.query(`SELECT display_name, gmail, public."Post".userid, public."Post".postid, post_text, time_posted, COALESCE(likes, 0) as likes, COALESCE(dislikes,0) as dislikes, gmail,
+            COALESCE(isLiked, false) as isLiked,COALESCE(isDisliked, false) as isDisliked
+                FROM public."Post" NATURAL LEFT JOIN
+                (SELECT count(liketype) as likes, postid FROM public."Like_Dislike"
+                WHERE postid in (SELECT postid FROM public."Post") and liketype = 1
+                GROUP BY postid
+                ) as likeQuery NATURAL LEFT JOIN
+                (SELECT count(liketype) as dislikes, postid FROM public."Like_Dislike"
+                WHERE postid in (SELECT postid FROM public."Post") and liketype = 0
+                GROUP BY postid
+                ) as dislikeQuery
+                NATURAL LEFT JOIN (SELECT postid, CASE liketype WHEN 0 THEN true END AS isDisliked FROM public."Like_Dislike" NATURAL JOIN public."User" WHERE gmail = $1 ) as getIsLiked
+                NATURAL LEFT JOIN (SELECT postid, CASE liketype WHEN 1 THEN true END AS isLiked FROM public."Like_Dislike" NATURAL JOIN public."User" WHERE gmail = $1 ) as getIsDisliked
+                NATURAL JOIN public."User"
+                WHERE userid in ((SELECT user2 as user FROM public."Friend" NATURAL JOIN (SELECT gmail, userid as user1 FROM public."User") as user2Gen WHERE gmail = $1)
+                UNION (SELECT user1 as user FROM public."Friend" NATURAL JOIN (SELECT gmail, userid as user2 FROM public."User") as user1Gen WHERE gmail = $1)) or gmail = $1
+                ORDER BY time_posted DESC
+             `, [email],
                 function(err, result)
                 {
                     client.release();
