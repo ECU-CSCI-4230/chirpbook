4f99c6f94a694abd7e8794f22f56d5575a7298d4
Zachary Catalano <catalanoz15@students.ecu.edu>
2019-02-14 14:10:40 -0500

    Add post management queries and tests

--- chirpbook-api/src/postmanagement.js
+++ chirpbook-api/src/postmanagement.js
@@ -84,7 +84,31 @@ class PostManagement
     {
         db.connect(function(client)
         {
-            client.query(`DELETE FROM public."Post" WHERE postid = $1`, [postid],
+            client.query(`UPDATE public."Post" SET post_text = '[Redacted]', userid = 0 WHERE postid = $1`, [postid],
+                function(err, result)
+                {
+                    client.release();
+                    if(err)
+                    {
+                        log.error(err);
+                    }
+                    if(result)
+                    {
+                        log.info(result);
+                        cb(result.rowCount);
+                    } else
+                    {
+                        cb(0);
+                    }
+                });
+        });
+    }
+
+    static removeUserPosts(userid, cb)
+    {
+        db.connect(function(client)
+        {
+            client.query(`UPDATE public."Post" SET post_text = '[Redacted]', userid = 0 WHERE userid = $1`, [userid],
                 function(err, result)
                 {
                     client.release();
@@ -123,6 +147,102 @@ class PostManagement
                     } else
                     {
                         cb(0);
+                    }
+                });
+        });
+    }
+
+    static getPostComments(postid, cb)
+    {
+        db.connect(function(client)
+        {
+            client.query(`SELECT commentid, postid, userid, comment_text, time_posted FROM public."Comment" WHERE postid = $1`, [postid],
+                function(err, result)
+                {
+                    client.release();
+                    if(err)
+                    {
+                        log.error(err);
+                    }
+                    if(result)
+                    {
+                        log.info(result);
+                        cb(result);
+                    } else
+                    {
+                        cb({rows: [], rowCount: 0});
+                    }
+                });
+        });
+    }
+
+    static getUserPosts(userid, cb)
+    {
+        db.connect(function(client)
+        {
+            client.query(`SELECT public."Post".userid, public."Post".postid, post_text, time_posted, COALESCE(likes, 0) as likes, COALESCE(dislikes,0) as dislikes
+            FROM public."Post" NATURAL LEFT JOIN
+            (SELECT count(liketype) as likes, postid FROM public."Like_Dislike"
+              WHERE postid in (SELECT postid FROM public."Post") and liketype = 1
+              GROUP BY postid
+            ) as likeQuery NATURAL LEFT JOIN
+            (SELECT count(liketype) as dislikes, postid FROM public."Like_Dislike"
+              WHERE postid in (SELECT postid FROM public."Post") and liketype = 0
+              GROUP BY postid
+            ) as dislikeQuery
+              WHERE userid=$1
+             `, [userid],
+                function(err, result)
+                {
+                    client.release();
+                    if(err)
+                    {
+                        log.error(err);
+                    }
+                    if(result)
+                    {
+                        log.info(result);
+                        cb(result);
+                    } else
+                    {
+                        cb({rows: [], rowCount: 0});
+                    }
+                });
+        });
+    }
+
+    static getFriendPosts(userid, cb)
+    {
+        db.connect(function(client)
+        {
+            client.query(`SELECT public."Post".userid, public."Post".postid, post_text, time_posted, COALESCE(likes, 0) as likes, COALESCE(dislikes,0) as dislikes FROM public."Post"
+            NATURAL LEFT JOIN
+            (SELECT count(liketype) as likes, postid FROM public."Like_Dislike"
+              WHERE postid in (SELECT postid FROM public."Post") and liketype = 1
+              GROUP BY postid
+            ) as likeQuery NATURAL LEFT JOIN
+            (SELECT count(liketype) as dislikes, postid FROM public."Like_Dislike"
+              WHERE postid in (SELECT postid FROM public."Post") and liketype = 0
+              GROUP BY postid
+            ) as dislikeQuery
+              WHERE userid in ((SELECT user2 as user FROM public."Friend" WHERE user1 = $1)
+                      UNION (SELECT user1 as user FROM public."Friend" WHERE user2 = $1)) or userid = $1
+
+             `, [userid],
+                function(err, result)
+                {
+                    client.release();
+                    if(err)
+                    {
+                        log.error(err);
+                    }
+                    if(result)
+                    {
+                        log.info(result);
+                        cb(result);
+                    } else
+                    {
+                        cb({rows: [], rowCount: 0});
                     }
                 });
         });
--- chirpbook-api/test/database/test-postmanagement.js
+++ chirpbook-api/test/database/test-postmanagement.js
@@ -1,6 +1,5 @@
-const {PostManagement, UserManagement, assert} = require("../common");
+const {PostManagement, UserManagement, LikeManagement, FriendManagement, CommentManagement, assert} = require("../common");
 
-// should use the created user after the functionality is added
 var userid = null;
 it('create a test user', function(done)
 {
@@ -12,7 +11,30 @@ it('create a test user', function(done)
     });
 });
 
-it('create post', function(done)
+var userid2 = null;
+it('create another test user', function(done)
+{
+    UserManagement.createUser('test@gmail.com', '', function(result)
+    {
+        assert.strictEqual(result.rowCount, 1);
+        userid2 = result.rows[0].userid
+        done();
+    });
+});
+
+var userid3 = null;
+it('create another test user', function(done)
+{
+    UserManagement.createUser('test2@gmail.com', '', function(result)
+    {
+        assert.strictEqual(result.rowCount, 1);
+        userid3 = result.rows[0].userid
+        done();
+    });
+});
+
+
+it('create post from user 1', function(done)
 {
     PostManagement.createPost(userid, 'a fake post', function(result)
     {
@@ -21,16 +43,16 @@ it('create post', function(done)
     });
 });
 
-it('create another post', function(done)
+it('create post from user 2', function(done)
 {
-    PostManagement.createPost(userid, 'a different fake post for the second test', function(result)
+    PostManagement.createPost(userid2, 'a different fake post for the second test', function(result)
     {
         assert.strictEqual(result, 1)
         done();
     });
 });
 
-it('create a post to edit', function(done)
+it('create a post to edit from user 1', function(done)
 {
     PostManagement.createPost(userid, 'we will edit this post', function(result)
     {
@@ -46,11 +68,22 @@ it('get user\'s recent post', function(done)
     PostManagement.getUserPosts(userid, function(result)
     {
         userPosts = result.rows;
-        assert.strictEqual(result.rowCount, 3)
+        assert.strictEqual(result.rowCount, 2)
         done();
     });
 });
 
+var user2Posts = [];
+
+it('get user2\'s recent post', function(done)
+{
+    PostManagement.getUserPosts(userid2, function(result)
+    {
+        user2Posts = result.rows;
+        assert.strictEqual(result.rowCount, 1)
+        done();
+    });
+});
 
 it('edit a post', function(done)
 {
@@ -62,6 +95,33 @@ it('edit a post', function(done)
         });
 });
 
+it('test add like', function(done)
+{
+    LikeManagement.addLike(userPosts[userPosts.length - 1].postid, userid3, 1, function(result)
+    {
+        assert.strictEqual(result.rowCount, 1)
+        done();
+    });
+});
+
+
+it('test add dislike', function(done)
+{
+    LikeManagement.addLike(userPosts[userPosts.length - 1].postid, userid, 0, function(result)
+    {
+        assert.strictEqual(result.rowCount, 1)
+        done();
+    });
+});
+
+it('test add dislike', function(done)
+{
+    LikeManagement.addLike(userPosts[userPosts.length - 1].postid, userid2, 0, function(result)
+    {
+        assert.strictEqual(result.rowCount, 1)
+        done();
+    });
+});
 
 it('check post was edited', function(done)
 {
@@ -72,22 +132,134 @@ it('check post was edited', function(done)
     });
 });
 
-it('remove all user posts', function(done)
+var commentid;
+it('test create comment', function(done)
 {
-    let numToProcess = userPosts.length;
-    userPosts.forEach(row =>
+    CommentManagement.createComment(userPosts[userPosts.length - 1].postid, null, userid, "hello!", function(result)
     {
-        PostManagement.removePost(parseInt(row.postid), function(result)
-        {
-            assert.strictEqual(result, 1);
-            numToProcess--;
-            if(numToProcess === 0)
-            {
-                done()
-            }
-        });
+        assert.strictEqual(result.rowCount, 1)
+        commentid = result.rows[0].commentid;
+        done();
     });
 });
+
+it('check get post comments', function(done)
+{
+    PostManagement.getPostComments(userPosts[userPosts.length - 1].postid, function(result)
+    {
+        assert.strictEqual(result.rowCount, 1);
+        done();
+    });
+});
+
+it('test delete comment', function(done)
+{
+    CommentManagement.deleteComment(commentid, function(result)
+    {
+        assert.strictEqual(result, 1);
+        done();
+    });
+});
+
+it('get all of user1\'s posts', function(done)
+{
+    PostManagement.getUserPosts(userid, function(result)
+    {
+        assert.strictEqual(parseInt(result.rows[result.rowCount - 1].dislikes), 2);
+        assert.strictEqual(parseInt(result.rows[result.rowCount - 1].likes), 1);
+        assert.strictEqual(parseInt(result.rows[result.rowCount - 2].dislikes), 0);
+        assert.strictEqual(parseInt(result.rows[result.rowCount - 2].likes), 0);
+        assert.strictEqual(result.rowCount, 2);
+        done();
+    });
+});
+
+it('test create friend', function(done)
+{
+    FriendManagement.createFriend(userid, userid2, function(result)
+    {
+        assert.strictEqual(result.rowCount, 1)
+        done();
+    });
+});
+
+it('get user1\'s friend\'s posts', function(done)
+{
+    PostManagement.getFriendPosts(userid, function(result)
+    {
+        assert.strictEqual(result.rowCount, 3);
+        done();
+    });
+});
+
+it('test delete friend', function(done)
+{
+    FriendManagement.deleteFriend(userid, userid2, function(result)
+    {
+        assert.strictEqual(result.rowCount, 1)
+        done();
+    });
+});
+
+it('test remove like', function(done)
+{
+    LikeManagement.removeLike(userPosts[userPosts.length - 1].postid, userid3, function(result)
+    {
+        assert.strictEqual(result.rowCount, 1)
+        done();
+    });
+});
+
+it('test remove dislike', function(done)
+{
+    LikeManagement.removeLike(userPosts[userPosts.length - 1].postid, userid, function(result)
+    {
+        assert.strictEqual(result.rowCount, 1)
+        done();
+    });
+});
+
+it('test remove dislike', function(done)
+{
+    LikeManagement.removeLike(userPosts[userPosts.length - 1].postid, userid2, function(result)
+    {
+        assert.strictEqual(result.rowCount, 1)
+        done();
+    });
+});
+
+it('remove all user posts', function(done)
+{
+    PostManagement.removeUserPosts(userid, function(result)
+    {
+        assert.strictEqual(result >= 1, true);
+
+        done()
+
+    });
+});
+
+it('remove all user2 posts', function(done)
+{
+    PostManagement.removeUserPosts(userid2, function(result)
+    {
+        assert.strictEqual(result >= 1, true);
+
+        done()
+
+    });
+});
+
+// it('remove all user3 posts', function(done)
+// {
+//     PostManagement.removeUserPosts(userid3, function(result)
+//     {
+//         assert.strictEqual(result >= 1, true);
+
+//         done()
+
+//     });
+// });
 
 it('delete user', function(done)
 {
@@ -95,6 +267,23 @@ it('delete user', function(done)
     {
         assert.strictEqual(result.rowCount, 1)
         done()
-    })
+    });
+});
 
+it('delete user2', function(done)
+{
+    UserManagement.deleteUser(userid2, function(result)
+    {
+        assert.strictEqual(result.rowCount, 1)
+        done()
+    })
+});
+
+it('delete user3', function(done)
+{
+    UserManagement.deleteUser(userid3, function(result)
+    {
+        assert.strictEqual(result.rowCount, 1)
+        done()
+    });
 });
