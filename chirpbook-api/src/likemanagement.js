9417a7b435dc2adb1fdf9f34a4a67142394677f7
Brandon Gachuz <gachuzb17@students.ecu.edu>
2019-02-21 17:15:09 -0500

    Refactored code structure and modified callback
    The callback return result was modified to be the
    rowCount

--- chirpbook-api/src/likemanagement.js
+++ chirpbook-api/src/likemanagement.js
@@ -1,72 +1,67 @@
 const db = require('./config/database')
 
-var log = require('console-log-level')({level: 'warn'})
+var log = require('console-log-level')({ level: 'warn' })
 
-class LikeManagement
-{
-    static addLike(postid, userid, liketype, callback)
-    {
-	db.connect(function(client)
-        {
+class LikeManagement {
+    static addLike(postid, userid, liketype, callback) {
+        db.connect(function (client) {
             client.query(`INSERT INTO public."Like_Dislike" (postid, userid, liketype)
 								VALUES ($1, $2, $3)`, [postid, userid, liketype],
-                function(err, result)
-                {
+                function (err, result) {
+
+                    console.log(result)
+
                     client.release()
-                    if(err)
-                    {
+                    if (err) {
                         log.error(err)
                     }
-                    if(result)
-                    {
+                    if (result) {
                         log.info(result)
-                        callback(result)
+                        callback(result.rowCount)
+                    } else {
+                        callback(0);
                     }
                 });
         });
     }
 
-    static removeLike(postid, userid, callback)
-    {
-        db.connect(function(client)
-        {
+
+
+    static removeLike(postid, userid, callback) {
+        db.connect(function (client) {
             client.query(`DELETE FROM public."Like_Dislike" WHERE postid = $1 AND userid = $2`,
-                         [postid, userid],
-                function(err, result)
-                {
+                [postid, userid],
+                function (err, result) {
                     client.release()
-                    if(err)
-                    {
+                    if (err) {
                         log.error(err)
                     }
-                    if(result)
-                    {
+                    if (result) {
                         log.info(result)
-                        callback(result)
+                        callback(result.rowCount)
+                    } else {
+                        callback(0);
                     }
                 });
         });
     }
 
-    static editLike(postid, userid, liketype, callback)
-    {
-        db.connect(function(client)
-        {
+    static editLike(postid, userid, liketype, callback) {
+        db.connect(function (client) {
             client.query(`UPDATE public."Like_Dislike" 
             SET liketype = $3
             WHERE postid = $1 and userid = $2`,
-                         [postid, userid, liketype],
-                function(err, result)
-                {
+                [postid, userid, liketype],
+                function (err, result) {
                     client.release()
-                    if(err)
-                    {
+                    if (err) {
                         log.error(err)
                     }
-                    if(result)
-                    {
+                    if (result) {
                         log.info(result)
-                        callback(result)
+                        callback(result.rowCount)
+                    } else {
+                        callback(0);
                     }
                 });
         });
