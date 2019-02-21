38fcdb2672f80ce952f162e8b0df9c94b28df5d1
Zachary Catalano <catalanoz15@students.ecu.edu>
2019-02-21 17:28:43 -0500

    Add api call to add a post

--- chirpbook-api/src/routes/posts.js
+++ chirpbook-api/src/routes/posts.js
@@ -7,6 +7,7 @@ const app = express();
 const router = express.Router();
 
 const PostManagement = require('../postmanagement');
+const UserManagement = require('../usermanagement');
 const jwt = require('jsonwebtoken');
 const jwt_decode = require('jwt-decode');
 
@@ -37,5 +38,43 @@ router.get('/posts/get_homepage', function(req, res)
         }
     })
 });
+
+router.post('/posts/add', function(req, res)
+{
+    var postText = req.body.post_text;
+    var gmail = jwt_decode(req.headers.authorization.split(' ')[1]).email;
+    if(gmail && postText)
+    {
+        UserManagement.getUser(gmail, userRows =>
+        {
+            if(userRows.length == 1)
+            {
+                PostManagement.createPost(userRows[0].userid, postText, function(post_count)
+                {
+                    if(post_count == 1)
+                    {
+                        res.status(201).json({
+                            success: true,
+                            err: null
+                        });
+                    } else
+                    {
+                        res.status(404).json({
+                            success: false,
+                            err: "Cannot add post"
+                        });
+                    }
+                });
+            } else
+            {
+                res.status(404).json({
+                    success: false,
+                    err: "Cannot add post, invalid user."
+                });
+            }
+        });
+    }
+})
+
 
 module.exports = router;
