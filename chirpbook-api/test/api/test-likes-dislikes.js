5552d644c8e8c890f808eb1d7c2d38f349a54b10
Brandon Gachuz <gachuzb17@students.ecu.edu>
2019-02-21 17:10:20 -0500

    Added tests to add, remove, and edit a like.

--- chirpbook-api/test/api/test-likes-dislikes.js
+++ chirpbook-api/test/api/test-likes-dislikes.js
@@ -1,24 +1,56 @@
 const assert = require('assert');
 
 const request = require('request');
-const {basepath} = require('../common');
+const { basepath } = require('../common');
 
 console.log(basepath)
 
-it('edit like', function(done){
+it('add like', function (done) {
+    const path = 'http://localhost:8080/api/v1//like';
+
+    var reqBody = { postid: 0, userid: 0, like_type: 1 }
+    request.post(path, {
+        url: path,
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(reqBody),
+    }, function (err, res) {
+        var body = JSON.parse(res.body)
+        console.log(body)
+        assert.strictEqual(true, body.success);
+        done();
+    })
+})
+
+it('edit like', function (done) {
     const path = 'http://localhost:8080/api/v1//like';
 
     var reqBody = { userid: 0, like_type: 1, postid: 0 }
-    
+
     request.post(path, {
         url: path,
-        headers: {'Content-Type': 'application/json'},
+        headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(reqBody),
-    }, function(err, res)
-        {
-            var body = JSON.parse(res.body)
-            console.log(body)
-            assert.strictEqual(true, body.success);
-            done();
+    }, function (err, res) {
+        var body = JSON.parse(res.body)
+        console.log(body)
+        assert.strictEqual(true, body.success);
+        done();
+    });
+})
+
+it('remove like', function (done) {
+    const path = 'http://localhost:8080/api/v1//like';
+
+    var reqBody = { userid: 0, like_type: 1, postid: 0 }
+
+    request.delete(path, {
+        url: path,
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(reqBody),
+    }, function (err, res) {
+        var body = JSON.parse(res.body)
+        console.log(body)
+        assert.strictEqual(true, body.success);
+        done();
     });
 })
