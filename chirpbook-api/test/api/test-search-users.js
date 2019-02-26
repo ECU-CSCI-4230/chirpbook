54c220ba4c41a33866cf722584665336e5561575
Brandon Gachuz <gachuzb17@students.ecu.edu>
2019-02-26 14:33:43 -0500

    Added tests for searching users

---
+++ chirpbook-api/test/api/test-search-users.js
@@ -0,0 +1,23 @@
+const assert = require('assert');
+
+const request = require('request');
+const { basepath } = require('../common');
+
+console.log(basepath)
+
+it('search user', function (done) {
+    const path = 'http://localhost:8080/api/v1//user';
+
+    var reqBody = {} // add some parameters
+
+    request.get(path, {
+        url: path,
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(reqBody),
+    }, function (err, res) {
+        var body = JSON.parse(res.body)
+        console.log(body) // Maybe delete this later!
+        assert.strictEqual(true, body.success);
+        done();
+    })
+})
