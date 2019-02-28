6f212005c9277ce3469e44662e398278c7a1dddf
Brandon Gachuz <gachuzb17@students.ecu.edu>
2019-02-28 17:17:15 -0500

    Commented out console logs

--- chirpbook-api/test/database/test-likemanagement.js
+++ chirpbook-api/test/database/test-likemanagement.js
@@ -2,7 +2,7 @@ const { LikeManagement, assert } = require("../common");
 
 it('test add like', function (done) {
     LikeManagement.addLike(0, 0, 0, function (result) {
-        console.log(result)
+        //console.log(result)
         assert.strictEqual(result, 1)
         done();
     });
