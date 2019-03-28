aac0cbd3bc205f1691d0f687add2c20f1c71ffd7
cperry92 <perryc17@students.ecu.edu>
2019-03-28 17:15:11 -0400

    login page near complete

--- chirpbook-web/src/Components/LoginPage.js
+++ chirpbook-web/src/Components/LoginPage.js
@@ -1,22 +1,146 @@
 import React, {Component} from 'react';
 
-import {withStyles} from '@material-ui/core';
-import Typography from '@material-ui/core/Typography';
+import {withStyles, List, Typography, TextField, Grid, TableBody, Button} from '@material-ui/core';
+import Divider from '@material-ui/core/Divider';
+import {ListItem, ListItemAvatar, ListItemText, IconButton} from '@material-ui/core';
+import Avatar from '@material-ui/core/Avatar';
+import {AccountCircle, CheckCircleSharp} from '@material-ui/icons';
+import DeleteIcon from '@material-ui/icons/Delete';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import AddIcon from '@material-ui/icons/Add';
 
-class LoginPage extends Component{
+
+import AuthHelpers from '../Auth/AuthHelpers.js'
+import withAuth from '../Auth/withAuth';
+
+const Auth = new AuthHelpers();
+
+const styles = theme => ({
+    root: {
+        backgroundColor: theme.palette.background.paper
+    }
+
+});
+
+class LoginPage extends Component
+{
+    constructor(props)
+    {
+        super(props);
+        this.state = {
+            email: '',
+            password: '',
+            errmsg: ''
+        };
+        this.submit = this.submit.bind(this)
+    }
+
+    handleChange = name => event =>
+    {
+        this.setState({[name]: event.target.value});
+    };
+
+    submit()
+    {
+
+        var e = this.state.email
+
+        if(e.search('@') !== -1)
+        {
+            
+            if(this.state.password.length >= 6)
+            {
+                const b = JSON.stringify({'gmail': this.state.email, 'password': this.state.password})
+                const options = {
+                    headers: {'Content-Type': 'application/json'},
+                    method: 'POST',
+                    body: b
+                };
+                //const tokenH = r.headers.get('x-auth-token');
+                fetch('http://localhost/api/v1/login', options).then(r => r.json())
+                    .then(data =>
+                    {
+                        console.log(data)
+                        if(data.err === null)
+                        {
+                            Auth.login(data.token, data.userid)
+                            this.props.history.replace('/home')
+                        } else if(data.err === "Enter a valid gmail.")
+                        {
+                            this.setState({errmsg: 'Enter a valid email.'})
+                        } else if(data.err === "Username or password is incorrect.")
+                        {
+                            this.setSate({errmsg: 'Username or password is incorrect.'})
+                        } 
+                    });
+            }
+        } else
+        {
+            this.setState({errmsg: 'Not a Valid Email'})
+        }
+    }
+
     render()
     {
         const {classes} = this.props;
 
         return (
             <React.Fragment>
+                <Grid
+                    container
+                    spacing={16}
+                    alignItems='center'
+                    alignContent='center'
+                    direction='column'
+                >
+                    <Grid item >
 
-            <Typography align="center" variant="h1">Please Login</Typography>
+
+                        {this.state.errmsg === "" ? null :
+                            <Typography variant="h4" color="error">
+
+                                {this.state.errmsg}
+
+                            </Typography>
+
+
+                        }
+                    </Grid>
+                    <Grid item >
+                        <TextField
+                            id="standard-name"
+                            label="Email"
+                            className={classes.textField}
+                            value={this.state.email}
+                            onChange={this.handleChange('email')}
+                            margin="normal"
+                        />
+                    </Grid>
+
+                    <Grid item >
+                        <TextField
+                            id="standard-password-input"
+                            label="Password"
+                            className={classes.textField}
+                            type="password"
+                            autoComplete="current-password"
+                            margin="dense"
+                            value={this.state.password}
+                            onChange={this.handleChange('password')}
+                        />
+                    </Grid>
+                    <Grid item>
+                        <Button onClick={this.submit} variant="contained" size="medium" color="primary" className={classes.margin}>
+                            Submit
+                    </Button>
+                    </Grid>
+                </Grid>
 
             </React.Fragment>
-        );
 
+        )
     }
 }
 
-export default LoginPage;
+// export default withAuth(withStyles(styles)(Register));
+export default withStyles(styles)(LoginPage);
